30f63127177d266a50ebaaf4f2d187cfe84ae53a
st-linhnguyen <linh.nguyen@supremetech.vn>
2020-08-19 10:05:15 +0700

    change header tag to div

--- src/shared/components/layout/Footer.tsx
+++ src/shared/components/layout/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <h1>This is Footer</h1>
+    <div>This is Footer</div>
   );
 };
 
--- src/shared/components/layout/Header.tsx
+++ src/shared/components/layout/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <h1>This is header</h1>
+    <div>This is header</div>
   );
 };
 
