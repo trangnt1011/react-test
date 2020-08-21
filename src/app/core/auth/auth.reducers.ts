a6061de913a309300d3ee1ce4a96068a8c34b854
st-linhnguyen <linh.nguyen@supremetech.vn>
2020-08-21 15:45:59 +0700

    update auth reducer state variable name

--- src/app/core/auth/auth.reducers.ts
+++ src/app/core/auth/auth.reducers.ts
@@ -1,7 +1,7 @@
 import * as types from '@app/core/constant/types';
 
 const initialState = {
-  accessToken: null
+  token: null
 };
 
 export default function(state = initialState, { type, payload }) {
@@ -9,7 +9,7 @@ export default function(state = initialState, { type, payload }) {
     case types.SET_TOKEN:
       return {
         ...state,
-        accessToken: payload.accessToken
+        token: payload.accessToken
       };
     default:
       return state;
