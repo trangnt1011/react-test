564f1b7b704dc7faea35da414615457905adcda8
st-linhnguyen <linh.nguyen@supremetech.vn>
2020-08-22 12:01:57 +0700

    add pages component

--- src/app/pages/page.routes.ts
+++ src/app/pages/page.routes.ts
@@ -1,9 +1,16 @@
 import articleRoutes from './articles/article.routes';
 import homeRoutes from './home/home.routes';
+import Pages from './Pages';
 
 const pageRoutes = [
-  ...articleRoutes,
-  ...homeRoutes
+  {
+    path: '',
+    element: Pages,
+    children: [
+      ...articleRoutes,
+      ...homeRoutes
+    ]
+  }
 ];
 
 export default pageRoutes;
