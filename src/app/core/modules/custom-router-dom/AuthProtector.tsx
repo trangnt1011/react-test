f3d6c6c46b1bc9c34a1ecac73a0f1a04399950ad
st-linhnguyen <linh.nguyen@supremetech.vn>
2020-08-23 23:56:33 +0700

    redirect to login if not authenticate

--- src/app/core/modules/custom-router-dom/AuthProtector.tsx
+++ src/app/core/modules/custom-router-dom/AuthProtector.tsx
@@ -1,6 +1,21 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+
+import Login from '@core/auth/containers/Login'
+
+function checkAuthentication(): boolean {
+  const userId = localStorage.getItem('USER_ID');
+  const token = localStorage.getItem('ACCESS_TOKEN');
+
+  if (userId && token) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 export const AuthProtector = (route) => {
-  const token = null;
-  return token ? <route.element /> : <div>AuthProtector component works!</div>;
+  return checkAuthentication() ?
+    <route.element /> :
+    <Route path="auth/login" element={<Login/>} />;
 };
