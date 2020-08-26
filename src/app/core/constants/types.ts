a687f8e8c59cdad4950b230b64b82d861d942905
quando1910 <dhquan1910@gmail.com>
2020-08-26 11:37:52 +0700

    make api types

--- src/app/core/constants/types.ts
+++ src/app/core/constants/types.ts
@@ -1,11 +1,8 @@
-const apiGotSuccess = (type) => {
-  return type + '_SUCCESS';
+const makeApiTypes = (type: string) => ({
+  [`${type}`]: type,
+  [`${type}_SUCCESS`]: `${type}_SUCCESS`,
+  [`${type}_ERROR`]: `${type}_ERROR`
+});
+export const TYPES = {
+  ...makeApiTypes('SIGN_IN')
 };
-
-const apiGotError = (type) => {
-  return type + '_ERROR';
-};
-
-export const SIGN_IN = 'SIGN_IN';
-export const SIGN_IN_ERROR = apiGotError(SIGN_IN);
-export const SIGN_IN_SUCCESS = apiGotSuccess(SIGN_IN);
