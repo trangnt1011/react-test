693d06430d826843fbf3564e0ac1ecb2d10bbe6d
Trang Nguyen T <trangnguyent.@MBPM0039.local>
2023-08-29 11:25:26 +0700

    Add banner

--- src/app/pages/home/containers/Home.tsx
+++ src/app/pages/home/containers/Home.tsx
@@ -4,7 +4,12 @@ import { useTranslation } from 'react-i18next';
 const Home = (): JSX.Element => {
   const { t } = useTranslation('common');
   return (
-    <div>{t('pages.homepage')}</div>
+    <>
+      <div>{t('pages.homepage')}</div>
+      <div>
+        <img src="assets/images/banner.png" alt="Banner" />
+      </div>
+    </>
   );
 };
 
